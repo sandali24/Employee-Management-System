21d2d0dbeb0c6ef17212681029c0da93f0f8c86f
sandali24 <sandalisiri58@gmail.com>
2024-03-19 19:24:16 +0530

    React Routes

--- EmployeeMS/src/App.jsx
+++ EmployeeMS/src/App.jsx
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './Components/Login'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './Components/Dashboard'
+import Home from './Components/Home'
+import Employee from './Components/Employee'
+import Category from './Components/Category'
+import Profile from './Components/Profile'
 
 function App() {
 
@@ -11,7 +15,12 @@ function App() {
      <BrowserRouter>
      <Routes>
       <Route path='/adminlogin' element={<Login />}></Route>
-      <Route path='/dashboard' element={<Dashboard />}></Route>
+      <Route path='/dashboard' element={<Dashboard />}>
+        <Route path='' element={<Home />}></Route>
+        <Route path='/dashboard/employee' element={<Employee />}></Route>
+        <Route path='/dashboard/category' element={<Category />}></Route>
+        <Route path='/dashboard/profile' element={<Profile />}></Route>
+      </Route>
       </Routes>
       </BrowserRouter>
     </>
